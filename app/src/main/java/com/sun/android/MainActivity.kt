b552c335fccb091ade821aeab6dd84f36fac40bd
ntmdev116 <74419663+ntmdev116@users.noreply.github.com>
2023-02-17 13:57:13 +0700

    Send data back to main

--- app/src/main/java/com/sun/android/MainActivity.kt
+++ app/src/main/java/com/sun/android/MainActivity.kt
@@ -5,29 +5,78 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     private val LOG_TAG = MainActivity::class.java.simpleName
 
+    // using registerForActivityResult because startActivityForResult is deprecated
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+
+            // Handle the result here
+            val data: Intent? = result.data
+            val requestCode = data?.getIntExtra(REQUEST_CODE, 0)
+            Log.d("DDDD", requestCode.toString())
+
+            if (requestCode == TEXT_REQUEST)
+            {
+                val reply = data.getStringExtra(SecondaryActivity.EXTRA_REPLY)
+                Log.d("DDDD", reply.toString())
+                mReplyHeadTextView.apply { visibility = View.VISIBLE }
+                mReplyTextView.apply {
+                    visibility = View.VISIBLE
+                    text = reply
+                }
+            }
+        }
+    }
+
     companion object {
+        const val TEXT_REQUEST = 1
         const val EXTRA_MESSAGE = "com.sun.android.extra.MESSAGE"
+        const val REQUEST_CODE = "RequestCode"
     }
 
     private val editText : EditText by lazy { findViewById(R.id.editText_main) }
+    private val mReplyHeadTextView: TextView by lazy { findViewById(R.id.text_header_reply) }
+    private val mReplyTextView: TextView by lazy { findViewById(R.id.text_message_reply) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    // call back for send button
     fun launchSecondaryActivity(view: View) {
         val message : String = editText.text.toString()
         Log.d(LOG_TAG, "Button clicked!");
         val intent = Intent(this, SecondaryActivity::class.java)
 
         intent.putExtra(EXTRA_MESSAGE, message)
-        startActivity(intent)
+
+        startForResult.launch(intent)
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/sun/android/SecondaryActivity.kt
+++ app/src/main/java/com/sun/android/SecondaryActivity.kt
@@ -1,12 +1,19 @@
 package com.sun.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SecondaryActivity : AppCompatActivity() {
+    private val editText : EditText by lazy { findViewById(R.id.editText_second) }
+    companion object {
+        const val EXTRA_REPLY = "com.sun.android.extra.REPLY"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +21,23 @@ class SecondaryActivity : AppCompatActivity() {
         val intent = intent
 
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
-        Log.d("DDD", message.toString())
         val textView : TextView = findViewById(R.id.text_message)
         textView.text = message
+    }
+
+    // call back for reply button
+    fun returnReply(view: View) {
+        // Get EditText text
+        val mReply = editText.text.toString()
+
+        // put extra in intent
+        val intent = Intent()
+        intent.putExtra(EXTRA_REPLY, mReply)
+        intent.putExtra(MainActivity.REQUEST_CODE, MainActivity.TEXT_REQUEST)
+
+        // set result
+        setResult(RESULT_OK, intent)
+        // finish to start main activity
+        finish()
     }
 }
